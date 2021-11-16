5f04221814770b8c57084ad9e406aef05654961a
rimi <73830753+rimi0108@users.noreply.github.com>
2021-11-16 18:21:23 +0900

    Feat: add login test
    - login unit test 구현
    - 로그인 성공, 이메일 오류, 비밀번호 오류, 키에러 test 구현

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,9 @@
+import bcrypt, jwt
+
 from django.test import TestCase, Client
 
 from users.models import User
+from my_settings import SECRET_KEY, ALGORITHM
 
 
 class SignUpTest(TestCase):
@@ -45,6 +48,61 @@ class SignUpTest(TestCase):
         data = {"email": "user2@gmail.com"}
         response = client.post(
             "/users/signup", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
+
+
+class LogInTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            email="user1@gmail.com",
+            password=bcrypt.hashpw(
+                "abcd1234!".encode("utf-8"), bcrypt.gensalt()
+            ).decode(),
+        )
+        self.token = jwt.encode({"id": user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_log_in_post_success(self):
+        client = Client()
+        data = {"email": "user1@gmail.com", "password": "abcd1234!"}
+        response = client.post(
+            "/users/login", data=data, content_type="application/json"
+        )
+        token = self.token
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "LOGIN_SUCCESS", "token": token})
+
+    def test_log_in_post_invalid_email_fail(self):
+        client = Client()
+        data = {"email": "user@gmail.com", "password": "abcd1234!"}
+        response = client.post(
+            "/users/login", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "USER_DOES_NOT_EXIST"})
+
+    def test_log_in_post_invalid_password_fail(self):
+        client = Client()
+        data = {"email": "user1@gmail.com", "password": "wrong_password"}
+        response = client.post(
+            "/users/login", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "WRONG_PASSWORD"})
+
+    def test_log_in_post_key_error_fail(self):
+        client = Client()
+        data = {"password": "abcd1234!"}
+        response = client.post(
+            "/users/login", data=data, content_type="application/json"
         )
 
         self.assertEqual(response.status_code, 400)
