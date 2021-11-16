c6183bb302e56e19b40541af4ee5a7f4db91a727
rimi <73830753+rimi0108@users.noreply.github.com>
2021-11-16 17:26:09 +0900

    Feat: add signup test
    - 유저 회원가입 unit test
    - 로그인 성공, 이메일 형식 틀림, 비밀번호 형식 틀림, 키 에러 시 test 작성

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from users.models import User
+
+
+class SignUpTest(TestCase):
+    def setUp(self):
+        User.objects.create(email="user1@gmail.com", password="abcd1234!")
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_sign_up_post_success(self):
+        client = Client()
+        data = {"email": "user2@gmail.com", "password": "abcd1234!"}
+        response = client.post(
+            "/users/signup", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {"message": "SIGNUP_SUCCESS"})
+
+    def test_sign_up_post_email_validation_fail(self):
+        client = Client()
+        data = {"email": "user2gmail.com", "password": "abcd1234!"}
+        response = client.post(
+            "/users/signup", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "EMAIL_FORMAT_ERROR"})
+
+    def test_sign_up_post_password_validation_fail(self):
+        client = Client()
+        data = {"email": "user2@gmail.com", "password": "abc123"}
+        response = client.post(
+            "/users/signup", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "PASSWORD_FORMAT_ERROR"})
+
+    def test_sign_up_post_key_error_fail(self):
+        client = Client()
+        data = {"email": "user2@gmail.com"}
+        response = client.post(
+            "/users/signup", data=data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
