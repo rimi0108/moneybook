659778d97df671044f4162cda55fcc73486ad472
rimi <73830753+rimi0108@users.noreply.github.com>
2021-11-16 17:58:37 +0900

    Feat: add user login
    - 유저 로그인 기능 구현
    - 없는 이메일로 로그인 시 에러 발생
    - 패스워드 맞지 않을 시 에러 발생
    - 로그인 시 토큰 발행

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-import json, bcrypt, re
+import json, bcrypt, re, jwt
 
 from django.views import View
 from django.http import JsonResponse
+
+from my_settings import SECRET_KEY, ALGORITHM
 
 from .models import User
 
@@ -37,3 +39,38 @@ class SignupView(View):
 
         except json.decoder.JSONDecodeError:
             return JsonResponse({"message": "JSONDecodeError"}, status=400)
+
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email = data["email"]
+            password = data["password"]
+
+            user = User.objects.get(email=email)
+
+            if not bcrypt.checkpw(
+                password.encode("utf-8"), user.password.encode("utf-8")
+            ):
+                return JsonResponse({"MESSAGE": "WRONG_PASSWORD"}, status=401)
+
+            access_token = jwt.encode({"id": user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+            return JsonResponse(
+                {"message": "LOGIN_SUCCESS", "token": access_token},
+                status=200,
+            )
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"MESSAGE": "JSONDecodeError"}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({"MESSAGE": "USER_DOES_NOT_EXIST"}, status=400)
+
+        except User.MultipleObjectsReturned:
+            return JsonResponse({"MESSAGE": "MULTIPLE_OBJECTS_RETURNED"}, status=400)
