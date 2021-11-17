016239ae7180d765b573c16b5e9d9f72cdb6e87c
rimi <73830753+rimi0108@users.noreply.github.com>
2021-11-17 15:26:35 +0900

    Feat: add get moneybook list
    - 유저 로그인 후 기록한 가계부 list 조회
    - 쿼리 파라미터에 따라 결과값 필터링
    - 총 입금 금액과 출금 금액 계산

--- moneybooks/models.py
+++ moneybooks/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 class MoneyBook(models.Model):
-    user     = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    amount   = models.DecimalField(max_digits=19, decimal_places=4, default=0)
-    date     = models.DateField()
+    TRANSACTION_TYPE = (("DEPOSIT", "DEPOSIT"), ("WITHDRAW", "WITHDRAW"))
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=19, decimal_places=2, default=0)
+    date = models.DateField()
+    type = models.CharField(max_length=8, choices=TRANSACTION_TYPE, default="WITHDRAW")
     property = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
-    memo     = models.CharField(max_length=200, null=True)
+    memo = models.CharField(max_length=200, null=True)
 
     class Meta:
         db_table = "moneybooks"
