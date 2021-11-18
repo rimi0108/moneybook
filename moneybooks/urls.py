d113ccd8e4457d82ec6a66a20d03bcdf12d4548f
Hyerim Choi <hyerimc858@gmail.com>
2021-11-18 18:09:20 +0900

    Feat: add restore moneybook - hard delete에서 soft delete로 변경하여 사용자가 다시 복원할 수 있도록 함

--- moneybooks/urls.py
+++ moneybooks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MoneyBookView, DetailMoneyBookView
+from .views import MoneyBookView, DetailMoneyBookView, RestoreMoneyBookView
 
 urlpatterns = [
     path("moneybook", MoneyBookView.as_view()),
     path("moneybook/<int:moneybook_id>", DetailMoneyBookView.as_view()),
+    path("moneybook/restore/<int:moneybook_id>", RestoreMoneyBookView.as_view()),
 ]
